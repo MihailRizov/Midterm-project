fb09243c9e688a3cd25564cb922afb51edbf6724
Gabriela Peycheva <gabriela_pei4eva@abv.bg>
2019-02-04 17:21:02 +0200

    add items and modify css

--- assets/scripts/itemStorage.js
+++ assets/scripts/itemStorage.js
@@ -165,7 +165,7 @@ var toolBox = {
 
 itemStorage.set('women',[]).set('man',[]).set('children',[]).set('sport', []).set('bags', []).set('shoes', []);
 
-const TITLE_WOMAN = ['Дамска рокля с панделка', 'Дамска блуза с къс ръкав', 'Дамски топ с акцент на рамото', 'Дамски топ с интересен гръб', 'Дамски къс топ','Дамски потник с връзки']
+const TITLE_WOMAN = ['Дамска рокля с панделка', 'Дамска блуза с къс ръкав', 'Дамски топ с акцент на рамото', 'Дамски топ с интересен гръб', 'Дамски къс топ','Дамски потник с връзки'];
 const TITLE_WOMAN_SRC = ['http://vmzona.com/product_pics/big/IMG_8545copy_1158043.jpg',
 'http://vmzona.com/product_pics/big/IMG_3423_1158348.JPG',
 'http://vmzona.com/product_pics/big/IMG_5730copy_1022816.jpg',
@@ -174,6 +174,51 @@ const TITLE_WOMAN_SRC = ['http://vmzona.com/product_pics/big/IMG_8545copy_115804
 'http://vmzona.com/product_pics/big/IMG_7939_1045619_1045619.jpg'];
 const COLOR_WOMAN = ['тъмно зелен', 'черен', 'черен', 'бял', 'зелен', 'розов'];
 const SIZE = ['M','L', 'XS', 'XS', 'S', 'XXL'];
+
+const TITLE_MAN = ['Мъжки дънки с изчистен дизайн', 'Мъжка блуза с дълъг ръкав', 'Мъжко спортно горнище',
+'Мъжки суитшърт с качулка', 'Мъжка жилетка с дълъг ръкав', 'Мъжки елек с качулка'];
+const TITLE_MAN_SRC = [ 'http://vmzona.com/product_pics/big/IMG_0529copy_1154296.jpg', 
+'http://vmzona.com/product_pics/big/IMG_7800copy_1157518.jpg', 
+'http://vmzona.com/product_pics/big/IMG_8074copy_1148350.jpg',
+'http://vmzona.com/product_pics/big/IMG_5269copy_1144111.jpg',
+'http://vmzona.com/product_pics/big/IMG_9126copy_1133146.jpg',
+'http://vmzona.com/product_pics/big/1_1132753.jpg'];
+const COLOR_MAN = ['сив', 'черен', 'черен', 'бордо', 'тъмно сив','син'];
+
+const TITLE_KIDS = ['Детска рокля с апликация', ' Детско долнище за момчета', 'Детски комплект за момичета',
+'Детска блуза с дълъг ръкав', 'Детска блуза с дълъг ръкав за момичета','Детско пончо'];
+const TITLE_KIDS_SRC = ['http://vmzona.com/product_pics/big/IMG_1863_1155590.jpg',
+'http://vmzona.com/product_pics/big/54337_1154247.jpg',
+'http://vmzona.com/product_pics/big/IMG_9772copy_1151842.jpg',
+'http://vmzona.com/product_pics/big/DSC_0010copy_1149571.jpg',
+'http://vmzona.com/product_pics/big/IMG_6677_1147348.JPG',
+'http://vmzona.com/product_pics/big/IMG_3572copy_1141567.jpg'];
+const COLOR_KIDS= ['прасковено', 'тъмно син','розов', 'бледо зелен', 'цикламено','червен']
+const KIDS_SIZE = ['6м', '9м', '1г', '1.5г', '3г', '4г'];
+
+const TITLE_BAGS = ['Дамска стилна чанта', 'Дамска стилна чанта', 'Дамска ефектна чанта с дълга дръжка',
+'Дамска кожена раница с преден джоб','Мъжка кожена чанта','Мъжка текстилна чанта'];
+const TITLE_BAGS_SRC = ['http://vmzona.com/product_pics/big/DSC_1793_1156516.jpg',
+'http://vmzona.com/product_pics/big/DSC_1727_1156508_1156508.jpg',
+'http://vmzona.com/product_pics/big/DSC_1669_1155896.jpg',
+'http://vmzona.com/product_pics/big/IMG_1569_1155195_1155195.jpg',
+'http://vmzona.com/product_pics/big/IMG_9511_1151540.jpg',
+'http://vmzona.com/product_pics/big/IMG_1400_1154895.jpg'];
+const BAGS_SIZE = ['стандартен', 'стандартен', 'стандартен','стандартен','стандартен','стандартен'];
+const COLOR_BAGS = ['син',' червен', 'сребрист', 'кафяв','черен','черен'];
+
+
+const TITLE_SHOES = ['Дамски маратонки с платформа', 'Мъжки зимни боти', 'Дамски маратонки с надписи',
+'Дамски боти с лачени панели и декоративни връзки',' Дамски спортни обувки с връзки контрастни кантове и орнамент отпред',
+'Мъжки текстилни маратонки'];
+const TITLE_SHOES_SRC=['http://vmzona.com/product_pics/big/IMG_8105copy_1157697.jpg',
+'http://vmzona.com/product_pics/big/IMG_8096copy_1157691.jpg',
+'http://vmzona.com/product_pics/big/IMG_8078copy_1157677.jpg',
+'http://vmzona.com/product_pics/big/WB043-7Blk_287688_781415.jpg',
+'http://vmzona.com/product_pics/big/2212-3Blue_287354_781425.jpg',
+'http://vmzona.com/product_pics/big/DSC_0883copy_1154246.jpg'];
+const SHOES_SIZE = ['37','43','38','36','38','45'];
+const COLOR_SHOES = ['черен', ' черен', ' черен', ' черен', 'син', 'черен'];
 
 class Item {
     constructor(title, imgSrc, size, price){
@@ -187,7 +232,8 @@ class Item {
     }
 }
 for (let index = 0; index < TITLE_WOMAN.length; index++){
-    itemStorage.get('women').push(new Item((toolBox.getNextElement(TITLE_WOMAN))(),(toolBox.getNextElement(TITLE_WOMAN_SRC))(), (toolBox.getNextElement(SIZE_WOMAN))(), (Math.random() * (200- 100) + 100).toFixed(2)));
+    itemStorage.get('women').push(new Item((toolBox.getNextElement(TITLE_WOMAN))(),(toolBox.getNextElement(TITLE_WOMAN_SRC))(), (toolBox.getNextElement(SIZE))(), (Math.random() * (200- 100) + 100).toFixed(2)));
 
 }
 console.log(itemStorage.get('women'));
+
